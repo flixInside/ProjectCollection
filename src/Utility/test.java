6571e16df50bf2deae59fa3047f7849eee163ea8
Flix1895 <felix@hallerbach.info>
2022-06-24 17:56:41 +0200

    Update test.java

--- src/Utility/test.java
+++ src/Utility/test.java
@@ -15,17 +15,18 @@ import Home.HomeScreen;
 
 public class test {
     
-
     public test() throws FontFormatException, IOException{
         // create input stream for FontAwesome icons
-        InputStream is = HomeScreen.class.getResourceAsStream("/FontAwesome/fontawesome-webfont.ttf");
+        InputStream is = test.class.getResourceAsStream("../FontAwesome/fontawesome-webfont.ttf");
         Font font = Font.createFont(Font.TRUETYPE_FONT, is);
         font = font.deriveFont(Font.PLAIN, 24f);
 
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
+        JLabel label = new JLabel("\u2654");
+        //label.setFont(font);
         frame.add(panel);
-        panel.add(new JLabel("\uf43f"));
+        panel.add(label);
         frame.setMinimumSize(new Dimension(100, 150));
         frame.setVisible(true);
     }
